5eba1b3cdb04a00700d48ac866a4fe1a3988afbe
Hassan Fathy <hassanfathy460@gmail.com>
2023-07-28 15:45:57 +0300

    install axios  and wiki api

--- src/App.js
+++ src/App.js
@@ -1,12 +1,33 @@
-import { useState } from "react";
-
+import { useEffect, useState } from "react";
+import axios from "axios";
+    
 export default function App() {
+    const [term, setTerm] = useState("");
+    const [result, setResult] = useState("");
+
+    useEffect(() => {
+        const search = async () => {
+            const respond = await axios.get("https://en.wikipedia.org/w/api.php", {
+                params: {
+                    action: "query",
+                    list: "search",
+                    origin: "*",
+                    format: "json",
+                    srsearch: term,
+                },
+            });
+        };
+        if (term) {
+            search();
+        };
+    });
+
     return (
         <div className="container">
             <div className="row">
                 <div className="my-3">
                     <label htmlFor="exampleFormControlInput1" className="form-label">Search Input</label>
-                    <input type="text" className="form-control" id="exampleFormControlInput1"></input>
+                    <input type="text" className="form-control" id="exampleFormControlInput1" onChange={(e) => setTerm(e.target.value)} value={term}></input>
                 </div>
             </div>
             <div className='row'>
