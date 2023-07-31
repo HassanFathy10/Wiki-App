1be7abd834ad86808b85ce6124fe77c9c2594e7d
Hassan Fathy <hassanfathy460@gmail.com>
2023-07-31 21:34:50 +0300

    get previous state by use ref and use effect to fix search issue

--- src/App.js
+++ src/App.js
@@ -1,51 +1,74 @@
-import { useEffect, useState } from "react";
-import axios from "axios";
-    
+import { useEffect, useState, useRef } from 'react';
+import axios from 'axios';
+
 export default function App() {
-    const [term, setTerm] = useState("javaScript");
-    const [debounceSearch, setDebounceSearch] = useState(term);
+    const [term, setTerm] = useState('javascript');
     const [result, setResult] = useState([]);
+    const prevStateTerm = useRef('');
 
     useEffect(() => {
-        const timeOut = setTimeout(() => {
-            setDebounceSearch(term);
-        }, 1200);
-        return () => {
-            clearTimeout(timeOut);
-        };
-    }, [term])
+        prevStateTerm.current = term;
+    }, [term]);
+
+    const prevTerm = prevStateTerm.current;
 
     useEffect(() => {
         const search = async () => {
-            const respond = await axios.get("https://en.wikipedia.org/w/api.php", {
+            const respond = await axios.get('https://en.wikipedia.org/w/api.php', {
                 params: {
-                    action: "query",
-                    list: "search",
-                    origin: "*",
-                    format: "json",
-                    srsearch: debounceSearch,
+                    action: 'query',
+                    list: 'search',
+                    origin: '*',
+                    format: 'json',
+                    srsearch: term,
                 },
             });
             setResult(respond.data.query.search);
         };
-        search();
-    }, [debounceSearch]);
-    
+        if (!result.length) {
+            search();
+        } else if (term !== prevTerm) {
+            const debounceSearch = setTimeout(() => {
+                if (term) {
+                    search();
+                }
+            }, 2000);
+            return () => {
+                clearTimeout(debounceSearch);
+            };
+        }
+    }, [term, result.length, prevTerm]);
+
     const fetchResult = result.map((el) => {
         return (
             <tr key={el.pageid}>
-                <th>-</th>
+                <td>-</td>
                 <td>{el.title}</td>
-                <td><span dangerouslySetInnerHTML={{__html: el.snippet}}></span></td>
+                <td>
+                    <span dangerouslySetInnerHTML={{ __html: el.snippet }} />
+                </td>
             </tr>
         );
     });
+
     return (
-        <div className="container">
-            <div className="row">
-                <div className="my-3">
-                    <label htmlFor="exampleFormControlInput1" className="form-label">Search Input</label>
-                    <input type="text" className="form-control" id="exampleFormControlInput1" onChange={(e) => setTerm(e.target.value)} value={term}></input>
+        <div className='container'>
+            <div className='row'>
+                <div className='col'>
+                    <div className='my-3'>
+                        <label htmlFor='exampleFormControlInput1' className='form-label'>
+                            Search Input
+                        </label>
+                        <input
+                            type='text'
+                            className='form-control'
+                            id='exampleFormControlInput1'
+                            onChange={(e) => setTerm(e.target.value)}
+                            value={term}
+                        />
+                    </div>
+                    <p>Current term: {term}</p>
+                    <p>Prev Term: {prevTerm}</p>
                 </div>
             </div>
             <div className='row'>
@@ -58,9 +81,7 @@ export default function App() {
                                 <th scope='col'>Desc</th>
                             </tr>
                         </thead>
-                        <tbody>
-                            {fetchResult}
-                        </tbody>
+                        <tbody>{fetchResult}</tbody>
                     </table>
                 </div>
             </div>
